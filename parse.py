47928f5496c775d9a036b40824320aa1b12298b0
shahastransh <shahastranshtiwari@gmail.com>
2025-11-22 12:53:15 +0530

    updated versions

--- parse.py
+++ parse.py
@@ -11,7 +11,7 @@ template = (
 )
 
 
-model = OllamaLLM(model = "llama3.1")
+model = OllamaLLM(model = "phi4-mini")
 
 def parse_with_ollama(dom_chunks, parse_description):
     prompt = ChatPromptTemplate.from_template(template)
